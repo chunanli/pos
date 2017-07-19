4507d5537b554a7f405871bd32ebae1f9641a04f
Chunan Li <chunanli@gmail.com>
2017-07-19 12:05:40 -0400

    Java 8 features support.

--- app/src/main/java/com/zz/pos/MainActivity.java
+++ app/src/main/java/com/zz/pos/MainActivity.java
@@ -2,6 +2,8 @@ package com.zz.pos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textview = findViewById(R.id.text);
+        final Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(view -> {
+            textview.setText(textview.getText() + " Click");
+        });
     }
 }
